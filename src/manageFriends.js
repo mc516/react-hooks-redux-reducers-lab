38085f5d77751fb4cbe7bd35e921c786ee9f7427
Mickey <mickeyc11111@gmail.com>
2022-12-21 09:56:12 -0500

    commit tree f1fdd5a5d9891b4a97e6f95b3911b49b5ead67b9

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,22 @@
-export function manageFriends(state, action) {
+const initialState = { friends: [] }
+const action = {
+  type: "friends/add",
+  payload: {
+    name: "Chrome Boi",
+    homewtown: "NYC",
+    id: 1
+  }
+}
+
+export function manageFriends(state = initialState, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add":
+      return { ...state, friends: state.friends.push(action.payload)};
+  
+    case "friends/remove":
+      return { ...state, friends: state.friends.filter(friend => {return friend.id !== action.payload}) };
+    default:
+      return state;
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,15 @@
 const initialState = { numberOfPresents: 0 };
+const action = {
+  type: "presents/increase",
+};
 
 export function managePresents(state = initialState, action) {
   // your code here
+ switch (action.type) {
+  case "presents/increase":
+    return {...state, numberOfPresents: state.numberOfPresents + 1 };
+  default:
+    return state;
+ }
+  
 }
